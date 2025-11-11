067b217af7b0bdd2137217890505c3356377f37f
Marcell Henrique Meneses Ribeiro <HenriR1020@gmail.com>
2025-11-11 20:15:36 -0300

    chore: update pnpm workspace configuration and remove outdated README (#2)

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended',
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {},
+  ignorePatterns: ['dist', 'node_modules'],
+}
